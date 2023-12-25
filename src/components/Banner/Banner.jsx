788de89e23c1dfb747eb7b91e99172fdff5b9fe8
Valeriy <rikksandr@Gmail.com>
2023-12-25 19:44:49 +0300

    transition part

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,25 @@
+import './BannerStyles.scss'
+import $ from "jquery";
 
+
+//a little jQuery for example :)
+let bannerMessage = $('.solving_container__banner__flex')
+bannerMessage.css('opacity', '0');
+$('.solving_container__banner__image').click(function (event){
+    event.preventDefault()
+    bannerMessage.css('opacity', '0.9');
+})
+$('.solving_container__banner__cancel').click(function (event){
+    event.preventDefault()
+    bannerMessage.css('opacity', '0');
+})
 export default function Banner(){
-
     return(
         <>
             <div className="solving_container__banner">
                 <div className="solving_container__banner__flex">
-                    <div className="solving_container__banner__message">Hi! I am Ben, your virtual assistant. How can I
-                        make
-                        your day better?
+                    <div className="solving_container__banner__message">
+                        Hi! I am Ben, your virtual assistant. How can I make your day better?
                     </div>
                     <div className="solving_container__banner__cancel">X</div>
                 </div>
